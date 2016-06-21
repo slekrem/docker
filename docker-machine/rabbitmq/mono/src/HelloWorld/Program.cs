75ad1391feba6b4e28c369715635b7678a22bf99
Stefan Merkel <merkel.s.ulm@gmail.com>
2016-06-21 17:57:20 +0200

    update

--- docker-machine/rabbitmq/mono/src/HelloWorld/Program.cs
+++ docker-machine/rabbitmq/mono/src/HelloWorld/Program.cs
@@ -1,12 +1,26 @@
 ﻿namespace HelloWorld
 {
 	using System;
+	using System.Configuration;
 	using RabbitMQ.Client;
 
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
+			foreach (string key in ConfigurationManager.AppSettings)
+			{
+				string value = ConfigurationManager.AppSettings[key];
+				Console.WriteLine("Key: {0}, Value: {1}", key, value);
+			}
+
+			foreach (var asd in Environment.GetEnvironmentVariables()) 
+			{
+				
+				
+				Console.WriteLine("Key: {0}, Value: {1}", asd.ToString(), "");
+			}
+
 			var factory = new ConnectionFactory() { HostName = "rabbitmq" };
 			using (var connection = factory.CreateConnection())
 			{
@@ -15,7 +29,7 @@
 					Console.WriteLine(channel.ToString());
 				}
 			}
-			
+
 			Console.WriteLine("Hello World!");
 		}
 	}
